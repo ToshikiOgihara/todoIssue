299380bdd33a29e86534438725951d85fc6ee7da
Toshiki_Ogihara <toshiki.ogihara14@gmail.com>
2024-06-15 10:44:10 +0000

    Todoリストのコントローラーを作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+  end
+  
+  # todoタスクを新しく作るためのページ用？
+  # newアクションは新規作成のみ。
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
